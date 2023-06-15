0ade4c73934cf4abc50d0a26a5b49b2d8ec6372e
jeddaa <fathiato@gmail.com>
2023-06-15 19:28:28 +0100

    finalizing the code

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,18 @@
+require_relative 'my_enumerable'
+
 class MyList
-  
+include MyEnumerable
+def initialize(theList)
+  @list = theList
+end
+ def each
+  @list.each
+ end
 end
 
+list = MyList.new([1, 2, 3, 4])
+
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
